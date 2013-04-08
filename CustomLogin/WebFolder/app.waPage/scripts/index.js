cccd3f4c2bc12be04d66584fd2f7526ee2a60504
Greg McCarvell <greg@gregmccarvell.com>
2013-04-07 23:16:03 -0400

    Initial commit

---
+++ CustomLogin/WebFolder/app.waPage/scripts/index.js
@@ -0,0 +1,27 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		CRM.logout();
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		if(WAF.directory.currentUser() == null){
+			window.location = '/';
+		}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
